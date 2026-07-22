520903d1f1dd1151a658c71c7ce1ecfe169693ab
Devendra kushwaha <ddevendrakushwaha9@gmail.com>
2026-07-22 15:39:10 +0530

    Fix React production routing

--- backend/index.js
+++ backend/index.js
@@ -25,17 +25,7 @@ app.use(cors(
 app.use(express.json());
 app.use(express.urlencoded({extended: true }))
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
-  });
-} else {
-  app.get("/", (req,res)=>{
-    res.send("shopnest backend is working properly!");
-  });
-}
 
 app.use('/api/auth', authentication);
 app.use('/api/products', require('./routes/productRoutes'));
@@ -43,7 +33,17 @@ app.use('/api/orders', require('./routes/orderRoutes'));
 app.use('/api/payment', require('./routes/paymentRoutes'));
 app.use('/api/analytics', require('./routes/analyticsRoutes'));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("ShopNest backend is working properly!");
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
